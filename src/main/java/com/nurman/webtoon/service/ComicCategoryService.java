617a47841b14fff3efd9bce260ba5f715a6b856c
nurman hadi <mnurmanhadi02@gmail.com>
2025-06-26 18:48:50 +0700

    remove import warning

--- src/main/java/com/nurman/webtoon/service/ComicCategoryService.java
+++ src/main/java/com/nurman/webtoon/service/ComicCategoryService.java
@@ -1,17 +1,12 @@
 package com.nurman.webtoon.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.nurman.webtoon.entity.ComicCategory;
-import com.nurman.webtoon.model.category.CategoryResponse;
-import com.nurman.webtoon.model.comic.ComicResponse;
 import com.nurman.webtoon.model.comicCategory.ComicCategoryRequest;
-import com.nurman.webtoon.model.comicCategory.ComicCategoryResponse;
 import com.nurman.webtoon.repository.CategoryRepository;
 import com.nurman.webtoon.repository.ComicCategoryRepository;
 import com.nurman.webtoon.repository.ComicRepository;
